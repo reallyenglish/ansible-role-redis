7adf27671197147b1b9b885a8982b1b8f0e503db
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-01-11 14:59:56 +0900

    introduce redis_config_default and redis_config
    this simplifies handling different redis version

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -10,7 +10,7 @@ redis_dir          = "/var/lib/redis"
 redis_log_dir      = '/var/log/redis'
 redis_port         = 6379
 redis_pidfile = '/var/run/redis/redis-server.pid'
-redis_logfile = '/var/log/redis/redis.log'
+redis_logfile = '/var/log/redis/redis-server.log'
 redis_password = 'password'
 
 case os[:family]
@@ -21,6 +21,7 @@ when 'freebsd'
   redis_config       = '/usr/local/etc/redis/redis.conf'
   redis_dir          = '/var/db/redis'
   redis_pidfile = '/var/run/redis/redis.pid'
+  redis_logfile = '/var/log/redis/redis.log'
 end
 
 redis_config_ansible = "#{ redis_config }.ansible"
@@ -64,12 +65,8 @@ describe file(redis_config_ansible) do
   its(:content) { should match Regexp.escape("pidfile #{redis_pidfile}") }
   its(:content) { should match Regexp.escape("logfile #{redis_logfile}") }
   its(:content) { should match Regexp.escape("dir #{redis_dir}") }
-  case os[:family]
-  when "ubuntu"
-    # NOOP
-  else
-    its(:content) { should match /tcp-backlog 512/ }
-  end
+  its(:content) { should match(/^port 6379/) } # default
+  its(:content) { should match(/^databases 17$/) } # non-default
 end
 
 describe service(redis_service_name) do
