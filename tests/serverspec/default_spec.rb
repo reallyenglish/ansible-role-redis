fdc760852d4be2047dc363c0baaf2f53be5e94fa
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-01-12 10:34:12 +0900

    Support OpenBSD

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -28,6 +28,15 @@ when "redhat"
   redis_config       = "/etc/redis.conf"
   redis_pidfile      = '/var/run/redis/redis.pid'
   redis_logfile      = '/var/log/redis/redis.log'
+when "openbsd"
+  redis_package_name = "redis"
+  redis_service_name = "redis"
+  redis_user         = "_redis"
+  redis_group        = "_redis"
+  redis_pidfile      = "/var/run/redis/redis.pid"
+  redis_dir          = "/var/redis"
+  redis_logfile      = nil
+  redis_log_dir      = nil
 end
 
 redis_config_ansible = "#{ redis_config }.ansible"
@@ -44,11 +53,13 @@ when 'freebsd'
   end
 end
 
-describe file(redis_log_dir) do
-  it { should be_directory }
-  it { should be_mode 755 }
-  it { should be_owned_by redis_user }
-  it { should be_grouped_into redis_group }
+if redis_log_dir
+  describe file(redis_log_dir) do
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by redis_user }
+    it { should be_grouped_into redis_group }
+  end
 end
 
 describe file(redis_dir) do
@@ -69,10 +80,20 @@ end
 describe file(redis_config_ansible) do
   it { should be_file }
   its(:content) { should match Regexp.escape("pidfile #{redis_pidfile}") }
-  its(:content) { should match Regexp.escape("logfile #{redis_logfile}") }
+  if redis_logfile
+    its(:content) { should match Regexp.escape("logfile #{redis_logfile}") }
+  end
   its(:content) { should match Regexp.escape("dir #{redis_dir}") }
   its(:content) { should match(/^port 6379/) } # default
   its(:content) { should match(/^databases 17$/) } # non-default
+end
+
+if redis_logfile
+  describe file(redis_logfile) do
+    it { should be_file }
+    it { should be_owned_by redis_user }
+    it { should be_grouped_into redis_user }
+  end
 end
 
 describe service(redis_service_name) do
